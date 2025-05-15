10d7b58e123b18c404ae914242973802b2815b6b
Corbeau <aurora.griffith@students.mq.edu.au>
2025-05-15 16:04:03 +1000

    deleting defunct assets

---
+++ Assets/Scripts/GridMap/ScriptableObjects/RoomOption.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RoomOption", menuName = "ScriptableObjects/TileMapping/RoomOption", order = 1)]
+public class RoomOption : CellOptionBase
+{
+    public RoomLayerMaskData roomMask;
+}
